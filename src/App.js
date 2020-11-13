45fa4bc0ecbb82d19d6ba38776830aca725d1966
Rishabh Poddar <rishabh.poddar@gmail.com>
2020-11-13 19:41:57 +0530

    adds supertokens on the frontend

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import SuperTokens, { getSuperTokensRoutesForReactRouterDom } from "supertokens-auth-react"
+import EmailPassword from "supertokens-auth-react/recipe/emailpassword";
+import Session from "supertokens-auth-react/recipe/session";
+import Home from "./Home";
+import { Switch, BrowserRouter as Router, Route } from "react-router-dom";
+
+SuperTokens.init({
+  appInfo: {
+    appName: "SuperTokens Demo App",
+    apiDomain: "http://localhost:3001",
+    websiteDomain: "http://localhost:3000"
+  },
+  recipeList: [
+    EmailPassword.init(),
+    Session.init()
+  ]
+});
+
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        {getSuperTokensRoutesForReactRouterDom()}
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
