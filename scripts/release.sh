52a7f249b9e26fa3872b65eea933de75d25d9b20
Rishabh Poddar <rishabh.poddar@gmail.com>
2021-02-16 12:43:00 +0530

    changes for deployment

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,7 +1,3 @@
-git checkout deploy
-git merge master
-git push
 tag=$1$(date '+%d-%m-%Y--%H-%M-%S')
 git tag $tag
 git push origin --tags
-git checkout master
--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -1,10 +1,8 @@
 docker run -d \
     --restart=always \
-    --name demo-app \
-    --label name=demo-app \
-    --label type=app \
-    --label mode=production \
-    --volume /home/ubuntu/supertokens-demo-react:/usr/src/app \
-    --publish 10000:10000 \
-    --publish 10001:10001 \
+    --name demo-app-thirdparty \
+    --label name=demo-app-thirdparty \
+    --volume /home/ubuntu/supertokens-demo-react-thirdparty:/usr/src/app \
+    --publish 10002:10000 \
+    --publish 10003:10001 \
     supertokens/demo
